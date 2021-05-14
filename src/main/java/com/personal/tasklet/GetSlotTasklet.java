95483fe904ffad03ccf2fd9ff1578f28f18d9b58
Abhishek Tiwari <abhishek.tiwari@paysafe.com>
2021-05-15 01:16:46 +0530

    fixed the use hardcoded url

--- src/main/java/com/personal/tasklet/GetSlotTasklet.java
+++ src/main/java/com/personal/tasklet/GetSlotTasklet.java
@@ -76,8 +76,7 @@ public class GetSlotTasklet implements Tasklet, StepExecutionListener {
 			headers.add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36");
 			HttpEntity<String> entity = new HttpEntity<>(headers);
 			String res = restTemplate.exchange(
-					"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=143001&date=09-05-2021",
-					HttpMethod.GET, entity, String.class).getBody();
+					url, HttpMethod.GET, entity, String.class).getBody();
 			System.out.println(res);
 			SlotResponse response =null;
 			try {
